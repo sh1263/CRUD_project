3ce27aa66cca59f4ee35854c6a4423cbfa3bfc68
Shoya Hayashi <naka12_0207@yahoo.co.jp>
2026-02-28 17:06:42 +0900

    init: Spring Bootプロジェクト作成

---
+++ src/main/java/com/example/regist/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.regist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public String showForm() {
+		return "hello";
+	}
+}
