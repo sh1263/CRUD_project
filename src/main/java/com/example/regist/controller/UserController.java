aaa72fb12f7bb243b66b513451d161a907f29aba
Shoya Hayashi <naka12_0207@yahoo.co.jp>
2026-03-08 23:44:59 +0900

    practice setup

--- src/main/java/com/example/regist/controller/UserController.java
+++ src/main/java/com/example/regist/controller/UserController.java
@@ -2,12 +2,23 @@ package com.example.regist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-
+	
 	@GetMapping("/")
 	public String showForm() {
 		return "hello";
+	}
+	
+	@PostMapping("/add")
+	public ModelAndView showReg(@RequestParam String name,
+								ModelAndView mav) {
+		mav.setViewName("register");
+		mav.addObject("name", name);
+		return mav;
 	}
 }
